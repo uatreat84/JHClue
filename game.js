f0c18b8badd28d7a6eff018e6950bf0cf58c6ce3
Joseph Allen <Joe@Josephs-Air.home>
2016-12-10 12:23:57 -0500

    Fixed issue where “Gun” was used in some places and “Revolver” in others causing Accusation to fail if the case had a Gun/Revolver

--- game.js
+++ game.js
@@ -48,7 +48,7 @@ module.exports = {
             var weaponCards = [
                     "Knife",
                     "Rope",
-                    "Gun",
+                    "Revolver",
                     "Candle Stick",
                     "Lead Pipe",
                     "Wrench"];
